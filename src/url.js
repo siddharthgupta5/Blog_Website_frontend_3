27cbc6abed13c2cc2945fce414b3d3b1233348d7
SIDDHARTHGUPTA <contactsid14@gmail.com>
2024-08-25 19:41:10 +0530

    prod_url

--- src/url.js
+++ src/url.js
@@ -1,2 +1,2 @@
-export const URL="http://localhost:5000"
-export const IMF="http://localhost:5000/images/"
+export const URL="https://blog-website-backend-1-ruv2.onrender.com"
+export const IMF="https://blog-website-backend-1-ruv2.onrender.com/images/"
